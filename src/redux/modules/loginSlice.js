4b75b99e162b926ce1e903914cafb160057c275c
eh-lee <fornewstack@daum.net>
2023-03-27 22:09:23 +0900

    Detail Set Commit

--- src/redux/modules/loginSlice.js
+++ src/redux/modules/loginSlice.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { instance } from "../../axios/api";
-import axios from 'axios';
 import { cookies } from "../../shared/cookies";
 
 const initialState = {
@@ -12,8 +11,6 @@ const initialState = {
         isError: null,
     }
 };
-//회원가입 -> post
-//로그인 -> post
 export const __signUp = createAsyncThunk(
     "SIGN_UP",
     async (payload, thunkAPI) => {
@@ -35,23 +32,15 @@ export const __login = createAsyncThunk(
     async (payload, thunkAPI) => {
         try {
             const data = await instance.post("/user/login", payload)
-            // console.log(data);
-            // console.log(data.data);
-
             cookies.set("token", data.headers.authorization, { path: "/" });
-            // return thunkAPI.fulfillWithValue(data.data)
             return thunkAPI.fulfillWithValue(payload)
             //-> 이때의 인자가 action object로 들어가는 것
             // return thunkAPI.fulfillWithValue(data) //-> A non-serializable value was detected in an action, in the path: `payload.headers`.
         }
         catch (error) {
             const errorMsg = error.response.data.errorMessage;
-            // console.log("에러메시지", errorMsg);
             alert(`${errorMsg}`);
-            // console.log(error);
-
             return thunkAPI.rejectWithValue(payload);
-            // return thunkAPI.rejectWithValue(error);
         }
     }
 );
