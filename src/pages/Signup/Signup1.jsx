a879a3ff32f8a0e05fe9bd9aff3f2f78382e834a
eh-lee <fornewstack@daum.net>
2023-03-27 14:04:20 +0900

    Compile Error Resolved Commit

--- src/pages/Signup/Signup1.jsx
+++ src/pages/Signup/Signup1.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 import { StButton, StFont, StSmfont } from "./Singstyled";
-import CheckboxLabels from "./CheckboxLabels";
-import instance from "../../axios/api";
+import CheckboxLabels from "../../components/CheckboxLabels";
+import { instance } from "../../axios/api";
 import { ESInput, useInput } from "../../hook/useInput";
 import { Navigate } from "react-router-dom";
 // import { useMutation } from 'react-query';
@@ -19,10 +19,9 @@ function Signup() {
 
   const navi = useNavigate();
 
-  const [user,setUser] = useState({
-    email:"",
-    password:"",
-
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
   });
 
   const chgInputHandler = (e) => {
@@ -34,9 +33,8 @@ function Signup() {
 
   const submitBtnHandler = (e) => {
     e.preventDefault();
-    navi('/signup2')
-    instance.post("/singup",user)
-
+    navi("/signup2");
+    instance.post("/singup", user);
   };
 
   //이 부분이 처리되서 먼저 입력안했는데 넘어감
@@ -55,41 +53,41 @@ function Signup() {
   // )
 
   return (
-  <>
+    <>
       <Helmet>
         <title>Login - Netflix</title>
       </Helmet>
-     
-    <StForm  onSubmit={submitBtnHandler}>
-      <div>
-        <StSmfont>1/3단계</StSmfont>
-        <br />
-        <StFont>
-          <h3>비밀번호를 설정해 멤버십을 시작하세요.</h3>
-          <p>몇 단계만 더 거치면 넷플릭스 가입 완료!</p>
-          <p>복잡한 단계는 모두 없앴습니다.</p>
-        </StFont>
-        <br />
-        <ESInput
-        type='email'
-        value={user.email}
-        name="email"
-        onChange={chgInputHandler}
-        placeholder="이메일 주소를 추가하세요" 
-        maxLength='15'
-        required
-        />
-        <br />
-        <ESInput
-        type='password'
-        value={user.password}
-        onChange={chgInputHandler}
-        name="password"
-        placeholder="비밀번호를 추가하세요(8 이상 20 미만)"
-        maxLength='20'        
-        required
-        />
-        {/* <ESInput
+
+      <StForm onSubmit={submitBtnHandler}>
+        <div>
+          <StSmfont>1/3단계</StSmfont>
+          <br />
+          <StFont>
+            <h3>비밀번호를 설정해 멤버십을 시작하세요.</h3>
+            <p>몇 단계만 더 거치면 넷플릭스 가입 완료!</p>
+            <p>복잡한 단계는 모두 없앴습니다.</p>
+          </StFont>
+          <br />
+          <ESInput
+            type="email"
+            value={user.email}
+            name="email"
+            onChange={chgInputHandler}
+            placeholder="이메일 주소를 추가하세요"
+            maxLength="15"
+            required
+          />
+          <br />
+          <ESInput
+            type="password"
+            value={user.password}
+            onChange={chgInputHandler}
+            name="password"
+            placeholder="비밀번호를 추가하세요(8 이상 20 미만)"
+            maxLength="20"
+            required
+          />
+          {/* <ESInput
         value={user.nick}
         onChange={chgInputHandler}
         name="nick"
@@ -97,9 +95,9 @@ function Signup() {
         maxLength='10'
         required
         /> */}
-        <CheckboxLabels />
-        <br />
-        <StButton>동의하고 계속</StButton>
+          <CheckboxLabels />
+          <br />
+          <StButton>동의하고 계속</StButton>
         </div>
       </StForm>
     </>
@@ -115,5 +113,5 @@ const StForm = styled.form`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  color:#fff
-`
+  color: #fff;
+`;
